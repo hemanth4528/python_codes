bc9dbcf8d4e24e5d3b571ed5cb6083ca27144c12
hemanth4528 <hemanth452528@gmail.com>
2025-11-04 21:22:57 +0530

    Range of armstrong numbers.py nearest prime number.py

---
+++ nearest prime number.py
@@ -0,0 +1,12 @@
+def isPrime(n):
+    if n<=1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input("Enter a number: "))
+m=int(input("Enter a number: "))
+for i in range(n,m+1):
+    if isPrime(i):
+        print(i,end=" ")
